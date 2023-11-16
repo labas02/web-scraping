e4ef54817edd85c1e84a36dc8816e8990d87a275
labas02 <zdenda.cejka112@seznam.cz>
2023-11-16 11:33:05 +0100

    asdfdasf

--- web-scraper.py
+++ web-scraper.py
@@ -1,10 +1,15 @@
 from bs4 import BeautifulSoup
 
+h2 = "cc"
 with open('index.html', 'r') as f:
     contents = f.read()
-
     soup = BeautifulSoup(contents, "html.parser")
 
     print(soup.h2)
     print(soup.p)
     print(soup.li)
+    h2 = soup.h2
+    f.close()
+
+    with open("text.txt", "w") as f:
+        f.writelines(h2)

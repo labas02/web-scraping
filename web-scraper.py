f579b99812edb2e7e77171a02a3f177748f0251a
labas02 <zdenda.cejka112@seznam.cz>
2023-11-22 06:54:02 +0100

    lool

--- web-scraper.py
+++ web-scraper.py
@@ -1,18 +1,30 @@
-from bs4 import BeautifulSoup
+import pandas as pd 
+import numpy as np 
+import sklearn
+from sklearn import linear_model
+from sklearn.utils import shuffle
 
-h2 = "cc"
-with open('index.html', 'r') as f:
-    contents = f.read()
-    soup = BeautifulSoup(contents, "html.parser")
+data = pd.read_csv("student-mat.csv", sep=";")
 
-    print(soup.h2)
-    print(soup.p)
-    print(soup.li)
-    h2 = soup.h2
-    f.close()
+data = data[["G1", "G2", "G3", "studytime", "failures", "absences",]]
 
-    with open("text.txt", "w") as f:
-        f.writelines("**********star of scrape**********\n")
-        f.writelines(h2)
-        f.writelines("\n")
-        f.writelines("***********end of scrape**********\n")
+predict = "G3"
+
+X = np.array(data.drop([predict], axis=1))
+Y = np.array(data[predict])
+
+X_train, X_test, Y_train, Y_test = sklearn.model_selection.train_test_split(X,Y,test_size=0.1)
+
+linear = linear_model.LinearRegression()
+
+linear.fit(X_train,Y_train)
+acc = linear.score(X_test,Y_test)
+print(acc)
+
+print("co: ", linear.coef_)
+print("intercept: ", linear.intercept_)
+
+predictions = linear.predict(X_test)
+
+for X in range(len(predictions)):
+   print(predictions[X], X_test[X],Y_test[X])
